c807c3b9e42b397a062863038f85af2d0932e3c1
quochuy43 <kuhuylevan@gmail.com>
2025-09-03 21:03:58 +0700

    continue update UI

--- src/components/ChatbotIcon.jsx
+++ src/components/ChatbotIcon.jsx
@@ -1,4 +1,4 @@
-import profileImg from "../assets/photo/profile_1.jpg";
+import profileImg from "../assets/photo/huy.jpg";
 
 const ChatbotIcon = () => {
     return (
@@ -7,7 +7,12 @@ const ChatbotIcon = () => {
             alt="Chatbot Icon"
             width={43}
             height={43}
-            style={{ borderRadius: "50%" }}
+            style={{
+                borderRadius: "50%",
+                objectFit: "cover",
+                border: "1px solid #fbaa5f",
+                boxShadow: "0 2px 6px rgba(0,0,0,0.2)", // Đổ bóng nhẹ
+            }}
         />
     );
 };
